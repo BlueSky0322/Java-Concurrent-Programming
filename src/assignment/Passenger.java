4743acae8abe6ea910052e3b95a9646befc76acf
BlueSky0322 <ryanng8628@gmail.com>
2022-05-31 18:09:53 +0800

    disembark(), board() synchronized functions and run() logic

--- src/assignment/Passenger.java
+++ src/assignment/Passenger.java
@@ -6,57 +6,62 @@
 package assignment;
 
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
  * @author Ryan Ng
  */
-public class Passenger implements Runnable {
+public class Passenger extends Thread {
 
-    //true indicates disembarking; false indicates boarding
-    boolean movementState;
-
-    private final int minPassengers = 10;
-    private final int maxPassengers = 50;
-
+    int passengersToBoard;
     Plane plane;
     PlaneStates state;
+    PassengerRange pr;
+    AtomicInteger passengerCount = new AtomicInteger();
 
     Passenger(Plane plane) {
+        this.passengersToBoard = (int) Math.floor(Math.random() * (PassengerRange.MAX.getValue() - PassengerRange.MIN.getValue() + 1) + PassengerRange.MIN.getValue());
         this.plane = plane;
     }
 
+    @Override
     public void run() {
-        //randomly generate number of passengers
-        int noOfPassenger = (int) Math.floor(Math.random() * (maxPassengers - minPassengers + 1) + minPassengers);
-        //this.movementState = true;
-        if (this.plane.state == PlaneStates.PASSENGERDISEMBARKING) {
-            //while (this.movementState) {
-                for (int i = 1; i <= noOfPassenger; i++) {
-                    System.out.println("Thread - Passenger " + i + ": I am disembarking from Plane " + this.plane.id + ".");
-                    try {
-                        Thread.sleep((long) (Math.random() * 1000)); // simulate passengers getting off at different intervals                
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(Passenger.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-                //this.movementState = false;
+        while (plane.state == PlaneStates.PASSENGERDISEMBARKING) {
+            System.out.println("Passengers waiting to disembark Plane " + plane.id + ": " + plane.noOfPassenger);
+            this.passengerCount.set(plane.noOfPassenger);
+            disembark(plane);
+            if (this.passengerCount.get() == 0)
+            {
+                System.out.println("All passengers have disembarked Plane " + plane.id + ".");
+                plane.state = PlaneStates.CLEANINGANDRESTOCKING;
             }
+        }
         
-        if (this.plane.state == PlaneStates.PASSENGERONBOARDING) {
-            //while (!this.movementState) {
-                for (int i = 1; i <= noOfPassenger; i++) {
-                    System.out.println("Thread - Passenger " + i + ": I am boarding Plane " + this.plane.id + ".");
-                    try {
-                        Thread.sleep((long) (Math.random() * 1000)); // simulate passengers getting off at different intervals                
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(Passenger.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-                //this.movementState = false;
+        while (plane.state == PlaneStates.PASSENGERONBOARDING) {
+            System.out.println("Passengers waiting to board Plane " + plane.id + ": " + this.passengersToBoard);
+            this.passengerCount.set(this.passengersToBoard);
+            board(plane);
+            if (this.passengerCount.get() == 0)
+            {
+                System.out.println("All passengers have boarded Plane " + plane.id + ".");
+                plane.state = PlaneStates.LEAVEGATE;
             }
+        }
+    }
+
+    synchronized void disembark(Plane plane) {
+        for (int i = 1; i <= plane.noOfPassenger; i++) {
+            System.out.println("Thread - Passenger " + i + ": I am disembarking from Plane " + plane.id + ".");
+            this.passengerCount.decrementAndGet();
+        }
+    }
+
+    synchronized void board(Plane plane) {
+        for (int i = 1; i <= this.passengersToBoard; i++) {
+            System.out.println("Thread - Passenger " + i + ": I am boarding Plane " + plane.id + ".");
+            this.passengerCount.decrementAndGet();
+        }
     }
 
 }
