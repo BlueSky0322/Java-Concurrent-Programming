e0d9517a4b6d1504c97aee5b4a6af056b5e5aa24
BlueSky0322 <ryanng8628@gmail.com>
2022-05-31 14:33:22 +0800

    added checkRunway(), checkGate()

--- src/assignment/Plane.java
+++ src/assignment/Plane.java
@@ -14,25 +14,35 @@ class Plane extends Thread {
     int id;
     PlaneStates state;
     Airport airport;
+    ATC atc;
     
-    Plane(int id, Airport airport) {
+    Plane(int id, Airport airport, ATC atc) {
         this.id = id;
         this.airport = airport;
+        this.atc = atc;
         this.state = PlaneStates.WANTTOLAND;
         System.out.println("Plane " + id + " has been generated.");
     }
 
     public void run() 
     { 
-        reachAirport();
         while (this.state == PlaneStates.WANTTOLAND) {
             System.out.println("Plane " + this.id + " wants to land! Requesting permission from ATC...");
+            atc.checkRunway(this);
+            
     }
+        while (this.state == PlaneStates.ONRUNWAYARRIVAL) {
+            atc.checkGate(this);
+        }
+        while (this.state == PlaneStates.ATGATE) {
+            System.out.println("Plane " + this.id + " getting ready to disembark its passengers...");
+            System.out.println("Gate ternminals are opening...");
+            this.state = PlaneStates.PASSENGERDISEMBARKING;
+        }
+        while (this.state == PlaneStates.PASSENGERDISEMBARKING){
+        }
+        while (this.state == PlaneStates.PASSENGERONBOARDING) {}
     }
     
     
-    private synchronized void reachAirport()
-    {
-        airport.addPlaneToQueue(this); 
-    }
 }
