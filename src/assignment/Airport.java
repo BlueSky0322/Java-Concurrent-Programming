a2e2140dd5d0cefa639031c9e7fa43769f752600
BlueSky0322 <ryanng8628@gmail.com>
2022-05-31 14:31:38 +0800

    remove everything

--- src/assignment/Airport.java
+++ src/assignment/Airport.java
@@ -22,45 +22,16 @@ public class Airport {
     //static int currentCapacity = 0;
     AtomicInteger airportCapacity = new AtomicInteger(0);
 
-    LinkedList<Plane> listOfPlanes;
+    //LinkedList<Plane> listOfPlanes;
 
     public Airport() {
-        listOfPlanes = new LinkedList<Plane>();
+        
     }
 
-    public void landPlaneOnRunway() {
-        Plane plane;
-        synchronized (listOfPlanes) {
-            System.out.println("ATC found a plane in queue.");
-            plane = (Plane) ((LinkedList<?>) listOfPlanes).pollFirst();
-        }
-    }
 
-    public void addPlaneToQueue(Plane p) {
-        System.out.println("Plane " + p.id + " is entering the airspace");
-        if (airportCapacity.get() == MAX_CAPACITY) {
-            System.out.println("Plane " + p.id + " is entering the airspace.");
-            synchronized (listOfPlanes) {
-//            if(listOfPlanes.size() == MAX_CAPACITY)
-//            {
-//            	System.out.println("No space available for Plane"+ p.id);
-//                try {
-//                    listOfPlanes.wait();
-//                } catch (InterruptedException ex) {
-//                    Logger.getLogger(Airport.class.getName()).log(Level.SEVERE, null, ex);
-//                }
-//                return ;
-//            }
 
-                ((LinkedList<Plane>) listOfPlanes).offer(p);
-                System.out.println("Plane " + p.id + " has been entered into waiting queue.");
-
-                if (listOfPlanes.size() == 1) {
-                    listOfPlanes.notify();
-                }
-            }
-        }
-    }
+    
+    
 
 //    public void addToAirportQueue(Plane p)
 //    {
