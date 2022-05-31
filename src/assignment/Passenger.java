961426b26b309f3dcf70f619bfa138a69d57b474
BlueSky0322 <ryanng8628@gmail.com>
2022-05-31 21:46:54 +0800

    added movementState boolean

--- src/assignment/Passenger.java
+++ src/assignment/Passenger.java
@@ -7,57 +7,64 @@ package assignment;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Ryan Ng
  */
 public class Passenger extends Thread {
-
+    boolean movementState = true;
+    
+    int passengerNo;
     int passengersToBoard;
     Plane plane;
     PlaneStates state;
     PassengerRange pr;
     AtomicInteger passengerCount = new AtomicInteger();
 
-    Passenger(Plane plane) {
+    Passenger(int passengerNo, Plane plane) {
+        this.passengerNo = passengerNo;
         this.passengersToBoard = (int) Math.floor(Math.random() * (PassengerRange.MAX.getValue() - PassengerRange.MIN.getValue() + 1) + PassengerRange.MIN.getValue());
         this.plane = plane;
     }
 
-    @Override
     public void run() {
-        while (plane.state == PlaneStates.PASSENGERDISEMBARKING) {
-            System.out.println("Passengers waiting to disembark Plane " + plane.id + ": " + plane.noOfPassenger);
-            this.passengerCount.set(plane.noOfPassenger);
-            disembark(plane);
+        while (this.plane.state == PlaneStates.PASSENGERDISEMBARKING && movementState) {
+            //System.out.println("Passengers waiting to disembark Plane " + plane.id + ": " + plane.noOfPassenger);
+            disembark(this.passengerNo, this.plane);
             if (this.passengerCount.get() == 0)
             {
                 System.out.println("All passengers have disembarked Plane " + plane.id + ".");
-                plane.state = PlaneStates.CLEANINGANDRESTOCKING;
+                plane.state = PlaneStates.PASSENGERONBOARDING;
+                return;
             }
         }
-        
+
         while (plane.state == PlaneStates.PASSENGERONBOARDING) {
             System.out.println("Passengers waiting to board Plane " + plane.id + ": " + this.passengersToBoard);
             this.passengerCount.set(this.passengersToBoard);
             board(plane);
-            if (this.passengerCount.get() == 0)
-            {
+            if (this.passengerCount.get() == 0) {
                 System.out.println("All passengers have boarded Plane " + plane.id + ".");
                 plane.state = PlaneStates.LEAVEGATE;
             }
         }
     }
 
-    synchronized void disembark(Plane plane) {
-        for (int i = 1; i <= plane.noOfPassenger; i++) {
-            System.out.println("Thread - Passenger " + i + ": I am disembarking from Plane " + plane.id + ".");
-            this.passengerCount.decrementAndGet();
+    void disembark(int passengerNo, Plane plane) {
+        try {
+                Thread.sleep((long) (Math.random() * 1000));
+                System.out.println("Passenger " + passengerNo + ": I am disembarking from Plane " + plane.id + ".");
+                this.passengerCount.decrementAndGet();
+                this.movementState = false;
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
     }
 
-    synchronized void board(Plane plane) {
+    void board(Plane plane) {
         for (int i = 1; i <= this.passengersToBoard; i++) {
             System.out.println("Thread - Passenger " + i + ": I am boarding Plane " + plane.id + ".");
             this.passengerCount.decrementAndGet();
