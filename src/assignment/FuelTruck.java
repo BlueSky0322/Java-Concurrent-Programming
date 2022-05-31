594c8cae0b80080f87a3cbd286bc0589ad96c775
BlueSky0322 <ryanng8628@gmail.com>
2022-06-01 03:11:53 +0800

    added Fuel Truck semaphore

---
+++ src/assignment/FuelTruck.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ *
+ * @author Ryan Ng
+ */
+public class FuelTruck {
+    Semaphore fuelTruckSem = new Semaphore(1);
+    
+    public void run()
+    {
+        
+    }
+}
