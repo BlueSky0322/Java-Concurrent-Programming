40fe09e94161431df36ffeda164ee51a31497227
BlueSky0322 <ryanng8628@gmail.com>
2022-06-01 03:11:30 +0800

    added Crew thread

---
+++ src/assignment/Crew.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+/**
+ *
+ * @author Ryan Ng
+ */
+public class Crew extends Thread {
+    
+    Plane plane;
+    private String threadName;
+
+    Crew(String name, Plane plane) {
+        this.plane = plane;
+        threadName = name;
+        System.out.println("Plane " + plane.id + ": " + threadName + " will perform cleaning duties.");
+    }
+    
+    public void run() {
+        try {
+            for (int i = 1; i < 4; i++) {
+                switch (i) {
+                    case 1:
+                        System.out.println("Plane " + plane.id + ": " + threadName + " is cleaning seats...");
+                        break;
+                    case 2:
+                        System.out.println("Plane " + plane.id + ": " + threadName + " is sweeping floor...");
+                        break;
+                    case 3:
+                        System.out.println("Plane " + plane.id + ": " + threadName + " empting trash...");
+                        break;
+                }
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + threadName + "interrupted.");
+        }
+        System.out.println(threadName + " has finished cleaning.");
+    }
+}
